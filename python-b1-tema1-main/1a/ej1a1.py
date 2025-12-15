ac196a84e807fcb218800523bdacaae5c58d9806
juretasaenzpena2-del <juretasaenzpena2@uoc.edu>
2025-12-15 17:16:00 +0100

    Mensaje

---
+++ python-b1-tema1-main/1a/ej1a1.py
@@ -0,0 +1,64 @@
+'''
+Enunciado:
+Implementa la función 'fibonacci(fibonacci_number)' que contenga el algoritmo
+de Fibonacci y reciba como parámetro un valor numérico entero llamado 
+'fibonacci_number'  y devuelva el valor de la serie Fibonacci en esa posición.
+Asimismo, si el valor no es numérico, o es menor a cero, se debe lanzar 
+una excepción ValueError("mensaje"), la cual puede incluir un mensaje que debería 
+corresponder con el tipo de error durante la validación.
+
+Parámetros:
+- fibonacci_number: Número entero positivo mayor a 0 que representa la
+posición en la serie Fibonacci.
+
+Ejemplo:
+    Entrada:
+    fibonacci(10)
+
+    Salida:
+    55
+
+Enunciat:
+Implementa la funció 'fibonacci(fibonacci_number)' que contingui l'algorisme
+de Fibonacci i rebi com a paràmetre un valor numèric enter anomenat
+'fibonacci_number' i torneu el valor de la sèrie Fibonacci en aquesta posició.
+Així mateix, si el valor no és numèric, o és menor a zero, cal llançar
+una excepció ValueError("missatge"), la qual pot incloure un missatge que hi hauria de
+correspondre amb el tipus d'error durant la validació.
+
+Paràmetres:
+- fibonacci_number: Nombre enter positiu superior a 0 que representa la
+posició a la sèrie Fibonacci.
+
+Exemple:
+     Entrada:
+     fibonacci(10)
+
+     Sortida:
+     55
+
+'''
+
+def fibonacci(fibonacci_number):
+    #Validacion de tipo
+    if not isinstance(fibonacci_number, int):
+        raise ValueError("El parámetro debe ser un numero entero")
+    
+    #Validacion de rango
+    if fibonacci_number < 0:
+        raise ValueError("El número no puede ser negativo")
+    
+    #Caso Base
+    if fibonacci_number == 0:
+        return 0
+    if fibonacci_number == 1:
+        return 1
+    
+    # Calculo iterativo
+    a, b = 0, 1
+    for _ in range(2, fibonacci_number +1):
+        a, b = b, a+b
+
+    return b
+    
+
